8f4fcb5db33d6ba8889774833a588e5088032550
sufiyanbmk <sufiyanbmk@gmail.com>
2022-12-26 21:44:16 +0530

    signup page

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState,useContext } from 'react';
 
 import Logo from '../../olx-logo.png';
+import { FirebaseContext } from '../../store/Context';
+import {useHistory} from 'react-router-dom'
 import './Signup.css';
 
 export default function Signup() {
+  const [username,setUsername] = useState('')
+  const [email,setEmail] = useState('');
+  const [phone ,setPhone] = useState('');
+  const [password,setPassword] = useState('');
+ 
+  
+  const {Firebase} = useContext(FirebaseContext)
+  let history = useHistory();
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    Firebase.auth().createUserWithEmailAndPassword(email,password).then((result)=>{
+      result.user.updateProfile({displayName:username})
+      Firebase.firestore().collection('users').add({
+        id:result.user.uid,
+        username:username,
+        phone:phone
+      }).then(()=>{
+        console.log('hiiii')
+        history.push("/login")
+      })
+
+    })
+  }
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={username}
+            onChange = {(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -24,7 +51,9 @@ export default function Signup() {
           <input
             className="input"
             type="email"
+            value={email}
             id="fname"
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             defaultValue="John"
           />
@@ -35,6 +64,8 @@ export default function Signup() {
             className="input"
             type="number"
             id="lname"
+            value={phone}
+            onChange ={(e) => setPhone(e.target.value)}
             name="phone"
             defaultValue="Doe"
           />
@@ -45,6 +76,8 @@ export default function Signup() {
             className="input"
             type="password"
             id="lname"
+            value={password}
+            onChange = {(e) => setPassword(e.target.value)}
             name="password"
             defaultValue="Doe"
           />
